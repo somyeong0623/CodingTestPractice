cae99778d391718257c58b7dbc1b31cb8a178ada
somyeong0623 <tthaud8487@naver.com>
2023-01-05 00:29:49 +0900

    packages 정리

---
+++ CodingTest/src/Programmers/StackQueue/copy/lv2_1.java
@@ -0,0 +1,27 @@
+package Programmers.StackQueue.copy;
+
+import java.util.Stack;
+
+//lv2_1. 올바른 괄호 
+class lv2_1{
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack<Character>();
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(c=='('){
+                stack.push(c);
+            }else{
+                if( stack.size()==0 || stack.peek()==')')
+                    return false;
+                else
+                    stack.pop();
+            }
+        }
+
+        if(stack.size()>0) // 올바른 괄호이면 여기까지 실행되었을때 stack이 비어 있을 것임.
+            answer=false;
+
+        return answer;
+    }
+}
--- CodingTest/src/Sep_week02/test.java
+++
@@ -1,8 +0,0 @@
-package Sep_week02;
-
-public class test {
-	public static void main(String[] args) {
-		int a = Integer.MAX_VALUE;
-		System.out.println(a);
-	}
-}
